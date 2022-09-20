683a9b30d53e4b5c05d776424f35d730ed759d58
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-09-20 16:51:35 +0200

    Add files via upload

---
+++ W11_WindowsSandbox.ps1
@@ -0,0 +1,123 @@
+################################################################################################
+# This script can be used to install Windows Sandbox on Windows 10 & 11                        #
+# Editor : Christopher Mogis                                                                   #
+# Date : 09/6/2022                                                                             #
+# Version 1.0                                                                                  #
+# - Initial version                                                                            #
+################################################################################################
+
+#Variables
+#$Date = Get-Date
+$ComputerInfo = Get-ComputerInfo
+
+    Write-Output ""
+    $WinSandbox = (Get-WindowsOptionalFeature -FeatureName Containers-DisposableClientVM -Online).State
+    $HyperV = (Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State
+    if($WinSandbox -eq "Enabled" -And $HyperV -eq "Enabled")
+    {
+        Write-Host "Windows Sandbox is enabled on $($ComputerInfo.CsName)" -ForegroundColor Green <# Action to perform if the condition is true #>
+        Write-Output ""
+        $configuration = $true
+        while ($configuration)
+        {
+          write-host "---------------------- Windows SandBox Configuration -----------------------"
+          write-host "1. Configure Windows Sandbox" -ForegroundColor Cyan
+          write-host "x. exit" -ForegroundColor Yellow
+          write-host "----------------------------------------------------------------------------"
+          $choix = read-host "Choose an action :"
+          switch ($choix)
+            {
+            "1" {
+                write-host "1. Configure Windows Sandbox" -ForegroundColor Cyan
+                }
+            "x" {$configuration = $false}
+            default {Write-Host "Invalid choice" -ForegroundColor Red}
+            }
+        }
+    }
+    else 
+    {
+        $continue = $true
+        while ($continue){
+          write-host "---------------------- Windows SandBox Installation -----------------------"
+          write-host "1. Prerequisites check" -ForegroundColor Cyan
+          write-host "2. Install Hyper-V" -ForegroundColor Cyan
+          write-host "3. Install Windows Sandbox" -ForegroundColor Cyan
+          write-host "x. exit" -ForegroundColor Yellow
+          write-host "---------------------------------------------------------------------------"
+          $choix = read-host "Choose an action :"
+          switch ($choix){
+            "1" {
+                #Prerequisites for used Windows SandBox
+                #Windows Version
+                Write-Output ""
+                Write-Output  "### Windows SandBox prerequisites check ###"
+                Write-Output ""
+                $WinVer = $ComputerInfo.WindowsProductName
+                if($WinVer -eq "Windows 10 Enterprise" -OR "Windows 10 Professional" )
+                {
+                    Write-Host "$WinVer installed on $($ComputerInfo.CsName) is compatible with the Windows Sandbox feature" -ForegroundColor Green <# Action to perform if the condition is true #>
+                }
+                else 
+                {
+                    Write-Host "$WinVer installed on $($ComputerInfo.CsName) is not compatible with the Windows Sandbox feature" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                }
+                #CPU Architecture
+                Write-Output ""
+                Write-Output  "### Check CPU Configuration ###"
+                Write-Output ""
+                $CoreCPU = $ComputerInfo.CsNumberOfLogicalProcessors
+                if($CoreCPU -ge "3"  )
+                {
+                    Write-Host "The CPU on $($ComputerInfo.CsName) is compatible with the Windows Sandbox feature" -ForegroundColor Green <# Action to perform if the condition is true #>
+                }
+                else 
+                {
+                    Write-Host "The CPU on $($ComputerInfo.CsName) is not compatible with the Windows Sandbox feature" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                }
+                #RAM Available
+                Write-Output ""
+                Write-Output  "### Check Memory Ram Installed ###"
+                Write-Output ""
+                $RamMemory = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object {"{0:N2}" -f ([math]::round($_.Sum / 1GB))}
+                if($RamMemory -lt "4.00")
+                {
+                    Write-Host "The Quantity of Memory Ram installed on $($ComputerInfo.CsName) is ready  with the Windows Sandbox feature" -ForegroundColor Green <# Action to perform if the condition is true #>
+                }
+                else 
+                {
+                    Write-Host "The Quantity of Memory Ram installed on $($ComputerInfo.CsName) is not ready with the Windows Sandbox feature" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                }
+                #RAM Available
+                Write-Output ""
+                Write-Output  "### Check HyperV feature ###"
+                Write-Output ""
+                $HyperV = (Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State
+                if($HyperV -eq "Enabled")
+                {
+                    Write-Host "Windows Hypervisor is enabled on your computer" -ForegroundColor Green <# Action to perform if the condition is true #>
+                }
+                else 
+                {
+                    Write-Host "WARNING - Windows Hypervisor is not enabled on your computer" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                }
+                Write-Output ""
+                }
+                #Option 2 : Install Hyper-V 
+            "2" {
+                Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+                Write-Host "After installation, please select option 3 to install Windows Sandbox" -ForegroundColor Green
+                Write-Output ""
+                }  
+                #Option 3 : Windows Sandbox installation
+            "3" {
+                    Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online
+                    Write-Host "After installation, please restart your computer and relaunch this script and select option 1 " -ForegroundColor Green
+                    Write-Output ""
+                }
+                #Option x : Exit menu
+            "x" {$continue = $false}
+            default {Write-Host "Choix invalide" -ForegroundColor Red}
+          }
+        }
+    }
